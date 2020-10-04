55d71bdb702ad5fbd5d510b45ae8ba52370d84d2
taskinkemal <kemaltaskin@yahoo.com>
2020-10-04 23:11:19 +0200

    more tests

---
+++ WebApplication.Test/QuestionsTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models.DbModels;
+using Moq;
+using WebApplication.Controllers;
+
+namespace WebApplication.Test
+{
+    [TestClass]
+    public class QuestionsTest
+    {
+        [TestMethod]
+        public async Task Get()
+        {
+            const int quizId = 56;
+            var questions = new List<Question>
+            {
+                new Question(),
+                new Question()
+            };
+
+            var questionManager = new Mock<IQuestionManager>();
+
+            questionManager.Setup(c => c.GetQuizQuestions(quizId))
+                .Returns(Task.FromResult(questions));
+
+            var sut = new QuestionsController(questionManager.Object);
+
+            var result = await sut.Get(quizId);
+
+            Assert.AreEqual(questions.Count, result.ToList().Count);
+        }
+    }
+}
---
+++ WebApplication.Test/QuizAttemptsTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using BusinessLayer.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models.DbModels;
+using Models.TransferObjects;
+using Moq;
+using WebApplication.Controllers;
+
+namespace WebApplication.Test
+{
+    [TestClass]
+    public class QuizAttemptsTest
+    {
+        [TestMethod]
+        public async Task Put()
+        {
+            var attempt = new QuizAttempt();
+
+            var result = await ExecutePut(2, 5, new CreateAttemptResponse
+            {
+                Attempt = attempt,
+                Result = CreateAttemptResult.Success
+            });
+
+            var resultObject = (QuizAttempt)result.Value;
+
+            Assert.AreSame(attempt, resultObject);
+        }
+
+        private async Task<JsonResult> ExecutePut(int userId, int quizId, CreateAttemptResponse response)
+        {
+            var quizAttemptsManager = new Mock<IQuizAttemptManager>();
+            quizAttemptsManager.Setup(c => c.CreateAttempt(userId, quizId))
+                .Returns<int, int>((u, q) =>
+                Task.FromResult(response));
+            var controller = new QuizAttemptsController(quizAttemptsManager.Object);
+            controller.Token = new AuthToken
+            {
+                Token = "token",
+                UserId = userId,
+                ValidUntil = DateTime.Now.AddDays(1),
+                IsVerified = true
+            };
+            var result = await controller.Put(quizId);
+
+            return result;
+        }
+    }
+}
